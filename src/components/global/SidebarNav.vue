9465a054a27c2a64a647abc8495c97819a640a76
Adrian Gawle <adriangawle@gmail.com>
2021-06-24 21:29:44 +0200

    #13 - Nav layout

--- src/components/global/SidebarNav.vue
+++ src/components/global/SidebarNav.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="flex">
-        <!-- Backdrop -->
         <div :class="isOpen ? 'block' : 'hidden'" @click="isOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
-        <!-- End Backdrop -->
 
         <div :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
             <div class="flex items-center justify-center mt-8">
